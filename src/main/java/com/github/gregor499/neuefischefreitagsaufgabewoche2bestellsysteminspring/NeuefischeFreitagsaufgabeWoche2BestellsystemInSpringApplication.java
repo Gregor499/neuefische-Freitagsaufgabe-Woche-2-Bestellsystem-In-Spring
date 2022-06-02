bc5ede3a597e15681f7ba2b3bb85875d3f37424c
Gregor499 <postfach@gregor-dirks.de>
2022-06-02 16:41:13 +0200

    Basis

---
+++ src/main/java/com/github/gregor499/neuefischefreitagsaufgabewoche2bestellsysteminspring/NeuefischeFreitagsaufgabeWoche2BestellsystemInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.gregor499.neuefischefreitagsaufgabewoche2bestellsysteminspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeFreitagsaufgabeWoche2BestellsystemInSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuefischeFreitagsaufgabeWoche2BestellsystemInSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gregor499/neuefischefreitagsaufgabewoche2bestellsysteminspring/NeuefischeFreitagsaufgabeWoche2BestellsystemInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gregor499.neuefischefreitagsaufgabewoche2bestellsysteminspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeFreitagsaufgabeWoche2BestellsystemInSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
